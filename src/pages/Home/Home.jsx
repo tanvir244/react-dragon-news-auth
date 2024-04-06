aaa5406cadfc4c1a10786a5d68d265f9721467ac
tanvir244 <tr30365s25@gmail.com>
2024-04-06 08:07:25 +0600

    folder structurize complete

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className="text-3xl font-bold">This is Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,10 @@
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>Right Side</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
